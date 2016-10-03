a2f95b4759a59da37d108b83fc09bdb17bddf583
Zihler <Oliver.Zihler@zuehlke.com>
2016-10-03 11:15:40 +0200

    deleted empty spec file

--- app/logout/logout.component.ts
+++ app/logout/logout.component.ts
@@ -10,20 +10,26 @@ import {LogoutService} from "./logout.service";
   }
 )
 export class LogoutComponent implements OnInit {
+  /** Message to be displayed if server responses*/
   message: string;
 
   constructor(private logoutService: LogoutService) {
 
   }
 
+  /** Logs an existing user out of the current session */
   public logout(): void {
-    this.logoutService.logout().then(message => this.message = message);
+    this.logoutService.logout().then(message => {
+      this.message = message;
+       localStorage.removeItem('user');
+    });
   }
 
   ngOnInit(): void {
     if (localStorage.getItem('user')) {
       this.logout();
-      localStorage.removeItem('user');
+    } else {
+      this.message = "No user to log out.";
     }
   }
 }
